bef9cdc739821b080ce1d27a417bd335b33b914e
julia-tatim <116753015+julia-tatim@users.noreply.github.com>
2025-04-06 16:10:46 -0300

    Organização React
    organização de pastas react e html da pagina SAQ

---
+++ Frontend/cs/src/components/pages/Saq.jsx
@@ -0,0 +1,34 @@
+function Saq() {
+  return (
+    <div className="saq">
+      <h1>Sobre Nós</h1>
+      <h2>Nossa Missão</h2>
+      <p>Acreditamos que a transparência e a eficiência financeira são fundamentais para que as ONGs possam causar um impacto ainda maior. Por isso, criamos uma plataforma que facilita a gestão financeira dessas organizações, ajudando-as a acompanhar suas receitas, despesas e relatórios com precisão e simplicidade.</p>
+      <img src="https://s3-alpha-sig.figma.com/img/13c6/4069/c3be0d92b4f971c23ae0fa3e95cbf871?Expires=1744588800&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=fwkR8hpdJyM0MSvRym5yKJV1Kct2MpiCgSSes6RmOGvU-3D7eNftOXOINQe9wpy9rHrkYXJHRY4VL0QJ1ZPTpeGg9xu86ZXI4G1NN7tva5aUcqId2PMAVBHDD0FDGyAWJj7fVC12PDlrYLFfidtoAV5m687cDV~DeEbfSr36ZjA12th7Cb0bNfpnAD1h~a9pp7M8T1q1RUrDbeaqSfRn2Rs~aJnnQH3JReLcQCEMFofAYFnujndz85fjPVItoA1qMbrnlMfKHfkih11ow4WwI9~cp3Xy4kCHy-Gg1FYTOQlYDGeHMisJ23HgYqu~6YqBHV-UZu9X8S0OiZ~ZTOZ7qw__"/>  
+
+      <h2>Nossos Valores</h2>
+      <ul>
+        <li>Transparência: Acreditamos que a confiança é construída com uma gestão financeira clara e acessível.</li>
+        <li>Impacto Social: Nossa prioridade é capacitar ONGs para que possam focar em suas causas.</li>
+        <li>Acessibilidade: Criamos uma plataforma intuitiva e acessível para organizações de todos os tamanhos.</li>
+        <li>Inovação: Utilizamos tecnologia para simplificar processos e otimizar a administração financeira.</li>
+      </ul>
+
+      <h2>O Que Oferecemos</h2>
+      <h3>Para Doadores</h3>
+      <p>Contribuir para uma causa é um ato de confiança, e queremos tornar essa experiência mais transparente. Nossa plataforma permite que você faça doações de forma simples e acompanhe exatamente como as ONGs estão utilizando esses recursos. Dessa forma, garantimos mais segurança e incentivamos a participação ativa no impacto social.</p>
+      <img src="https://s3-alpha-sig.figma.com/img/ada8/eed5/099b06f20073ae13f3dcee61372adba7?Expires=1744588800&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=YQaIUZlR8ddZumx-o8-xyQ7ZVFPDxH2YEQkonkvnPY9NxKba~cozQeuqcKZZuwawWVXBgzc-LCCikf1tOFJNYWljc9-ESXPEp~X-mQCXdFI6oD4b0BK~v09em~r~L1LRzF1dJlesqYN21iuJ78-OmuZ90RZ8quvXBmMiG~mI4ypLnJAs620phpGll3ITwrs1Rp8CFK9FqEnDBlFyA9NO3on3SWVpyXsWMkkxEgdea~QdihQDSmPTPfTDFwjYs3fdBoZJYtBDMXErs4WSBy9AaM6RXgkYQv0dzjDNt~I4eD7wCfZClX2kBKz-QJdPV3qMfX2XEsy2VwrPI1dtiCOsmg__"/>
+      <h3>Para ONGs</h3>
+      <p>Facilitamos a gestão financeira da sua organização com ferramentas intuitivas e completas:</p>
+      <ul>
+        <li>Painel de Controle Financeiro: Acompanhe receitas, despesas e relatórios detalhados.</li>
+        <li>Gráficos e Relatórios: Visualize de forma clara como os recursos estão sendo aplicados.</li>
+        <li>Transparência com Doadores: Exiba dados sobre os investimentos feitos com as doações, aumentando a credibilidade da sua ONG.</li>
+        <li>Nosso objetivo é conectar organizações e doadores, garantindo que cada contribuição tenha um impacto real e mensurável.</li>
+      </ul>
+      <img src="https://s3-alpha-sig.figma.com/img/2bb0/b548/a22b328aa69e63748dfd9a29084f62d6?Expires=1744588800&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=VuqrBl6vlhi8Crmx6KRy9rHmByY8kEg4F0OqQECiQKR7gSIv2Y4q68JccUe1NZH11v0wyk2k8i2BfCI6gB8dcMWEle6PSAsdBmANYpthvyCcKmpTi9BlJgRvMeRpUFpv3wApZ3ML5rBmAeM3Yoft-Tj-LNtJxwsHfD8JbXQKeZabj3XbN9XdyAuaUrXzKD09Ou1Eo6oOFagBbZcpVMZex5F33jJm3t0h91-XAleQ3BnhNgEHcK0DoIxCxopBvD70TSItCOra-VOMmDK4-vUrwxr6z4Zu~oF1jmjbajV2mV~j1ZHT-KbF4~kXV9AJI5-6~LWMQ8YVeAwfNmSfEMx6AQ__"/>
+    </div>
+  );
+}
+
+export default Saq
