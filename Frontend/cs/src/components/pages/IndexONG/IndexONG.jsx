afdcd580b07c96452481a301de4107fa9a821342
EricBinek <ericbineksant@gmail.com>
2025-04-14 01:10:40 -0300

    IndexONG
    Estrutura da pagina que vai ser o index da ong

---
+++ Frontend/cs/src/components/pages/IndexONG/IndexONG.jsx
@@ -0,0 +1,33 @@
+import './indexONG.css';
+
+
+const IndexONG = () => {
+  return (
+    <div className="ong-container">
+      <Navbar />
+
+      <main className="main-content">
+        <section className="header-box">
+          <h1>Página Central da ONG</h1>
+          <p>Bem-vindo! Escolha uma das opções abaixo:</p>
+        </section>
+
+        <section className="card-grid">
+          <a href="/relatorios" className="card">
+            <h2>Relatórios<br />e<br />Gráficos</h2>
+          </a>
+          <a href="/cadastro" className="card">
+            <h2>Cadastro<br />Colaboradores<br />e<br />Beneficiários</h2>
+          </a>
+          <a href="/registros" className="card">
+            <h2>Registros<br />Entrada/Saída</h2>
+          </a>
+        </section>
+      </main>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default IndexONG;
