52c7d16ed4f8421391ff00349b0e7975e63f7d44
Juan Servelo <91964551+JuanServelo@users.noreply.github.com>
2025-04-14 19:04:59 -0300

    criação do database

--- Backend/ControleSolidario/src/main/kotlin/com/example/model/Ong.kt
+++ Backend/ControleSolidario/src/main/kotlin/com/example/model/Ong.kt
@@ -1,6 +1,14 @@
 package com.exemplo.model
 
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "ongs")
 data class Ong(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0,
+
     val nome: String,
     val cnpj: String,
     val cep: String,
