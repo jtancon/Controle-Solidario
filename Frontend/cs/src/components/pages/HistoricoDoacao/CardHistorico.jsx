3e165699d3dca6084f10f7b83289e1e4f4c839b2
julia-tatim <116753015+julia-tatim@users.noreply.github.com>
2025-05-18 20:31:05 -0300

    index da ong
    index da ong completo, finalização do index do usuario e algumas alterações menores em outras paginas

--- Frontend/cs/src/components/pages/HistoricoDoacao/CardHistorico.jsx
+++ Frontend/cs/src/components/pages/HistoricoDoacao/CardHistorico.jsx
@@ -4,11 +4,14 @@ function CardHistorico({ nome, data, valor, imagem }) {
   return (
     <div className="cardHistorico">
       <img className="imgHist" src={imagem || "src/assets/ONGS.jpg"} alt="Icone ong" />
-      <div className="textoCard">
-        <p className="nomeONG">{nome || "ONG"}</p>
-        <p className="dataHora">{data || "XX/XX/XXXX"}</p>
+      <div className="containerHist">
+        <div className="textoCard">
+          <p className="nomeONG">{nome || "ONG"}</p>
+          <p className="dataHora">{data || "XX/XX/XXXX"}</p>
+        </div>
+        <p className="valor">R${valor?.toFixed(2) || "0,00"}</p>
       </div>
-      <p className="valor">R${valor?.toFixed(2) || "0,00"}</p>
+      
     </div>
   );
 }
