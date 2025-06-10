ef1fab9955f5455c80cc19518c7186b218a2fda8
julia-tatim <116753015+julia-tatim@users.noreply.github.com>
2025-06-09 23:12:02 -0300

    Revert "Merge pull request #14 from jtancon/main"
    This reverts commit 73aeef8d362b2b6ae72e394759f353a3b227ad47, reversing
    changes made to 60155151c5e7b2862e8217e5167311b7798ff930.

--- Frontend/cs/src/components/pages/HistoricoDoacao/CardHistorico.jsx
+++ Frontend/cs/src/components/pages/HistoricoDoacao/CardHistorico.jsx
@@ -1,19 +1,17 @@
 import "./HistoricoDoacao.css";
 
-// O componente recebe 'nome' e 'imagem' como propriedades (props)
 function CardHistorico({ nome, data, valor, imagem }) {
   return (
     <div className="cardHistorico">
-      {/* Aqui, ele usa a 'imagem' recebida. Se a imagem não for fornecida, ele usa uma imagem padrão. */}
       <img className="imgHist" src={imagem || "src/assets/ONGS.jpg"} alt="Icone ong" />
       <div className="containerHist">
         <div className="textoCard">
-          {/* Aqui, ele exibe o 'nome' recebido. Se o nome não for fornecido, ele exibe "ONG". */}
           <p className="nomeONG">{nome || "ONG"}</p>
           <p className="dataHora">{data || "XX/XX/XXXX"}</p>
         </div>
-        <p className="valor">R${valor?.toFixed(2).replace('.', ',') || "0,00"}</p>
+        <p className="valor">R${valor?.toFixed(2) || "0,00"}</p>
       </div>
+      
     </div>
   );
 }
